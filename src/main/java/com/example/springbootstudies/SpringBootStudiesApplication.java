173f6ee4527ee1fcc38355aab92d9ac775a435b5
henriquewallace <henrique.wallace@townsq.com.br>
2023-03-16 22:00:25 -0300

    First commit

---
+++ src/main/java/com/example/springbootstudies/SpringBootStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootstudies/SpringBootStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
