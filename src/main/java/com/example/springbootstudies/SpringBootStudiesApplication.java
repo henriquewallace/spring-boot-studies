f1af980554e8be386bb41f9f23ba919dc0e5e889
henriquewallace <henrique.wallace@hotmail.com>
2023-03-17 00:07:36 -0300

    Create Quote and Value record classes and consume an rest web service

--- src/main/java/com/example/springbootstudies/SpringBootStudiesApplication.java
+++ src/main/java/com/example/springbootstudies/SpringBootStudiesApplication.java
@@ -1,13 +1,37 @@
 package com.example.springbootstudies;
 
+import com.example.springbootstudies.consumingrest.Quote;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SpringBootStudiesApplication {
 
+    private static final Logger log = (Logger) LoggerFactory.getLogger(SpringBootStudiesApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStudiesApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate
+    restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject("http://localhost:8080/api/random", Quote.class);
+            log.info(quote.toString());
+        };
     }
 
 }
---
+++ src/main/java/com/example/springbootstudies/consumingrest/Quote.java
@@ -0,0 +1,7 @@
+package com.example.springbootstudies.consumingrest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Quote(String type, Value value) {
+}
